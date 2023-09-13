50eefc9deed3ce80ffe8aaaf03f2c4529f3c3abc
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2023-09-13 20:26:06 +0900

    feat: 프로젝트 초기 구성

---
+++ src/main/java/com/example/samplespringboot3javamaven/SampleSpringBoot3JavaMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot3javamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBoot3JavaMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleSpringBoot3JavaMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplespringboot3javamaven/SampleSpringBoot3JavaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplespringboot3javamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBoot3JavaMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
