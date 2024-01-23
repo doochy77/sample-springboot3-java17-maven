b2d405a6067fb7afd14281a7a674edb35f476ed9
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-01-23 18:15:35 +0900

    feat: 샘플 도커파일 추가

--- src/test/java/com/example/samplespringboot3javamaven/SampleSpringBoot3JavaMavenApplicationTests.java
+++ src/test/java/com/example/samplespringboot3javamaven/SampleSpringBoot3JavaMavenApplicationTests.java
@@ -1,9 +1,8 @@
 package com.example.samplespringboot3javamaven;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SampleSpringBoot3JavaMavenApplicationTests {
 
     @Test
